72b55aef6f3782ad8dea7f1f8d1bea97f618b273
yangyanbin <yangyanbin@shandagames.com>
2018-01-23 13:31:50 +0800

    add murmurhash sample

--- samples/hash.cpp
+++ samples/hash.cpp
@@ -12,5 +12,11 @@ int main() {
 
     std::cout << "abd : " << gdp::gdu::MurmurHash64("abd", 3, seed) << std::endl;
 
+    uint64_t input1 = 16791;
+    uint64_t hash2 = gdp::gdu::MurmurHash64(&input1, 8, seed);
+    std::cout << input1 << ": " << hash2 << std::endl;
+
+    uint64_t input2 = 16792;
+    std::cout << "16792 : " << gdp::gdu::MurmurHash64(&input2, 8, seed) << std::endl;
     return 0;
 }
