a6bd22e8f7d234513b87193145cfe2f939ecf1fa
yangyanbin <yangyanbin@shandagames.com>
2018-03-12 20:04:35 +0800

    change to_int to template

--- include/to_int.hpp
+++ include/to_int.hpp
@@ -9,16 +9,9 @@
 namespace gdp {
 namespace gdu {
 
-inline int to_int(const std::string& input) {
-    int ret = 0;
-    std::stringstream ostr;
-    ostr << input;
-    ostr >> ret;
-    return ret;
-}
-
-inline uint32_t to_uint32(const std::string& input) {
-    uint32_t ret = 0;
+template<typename T>
+inline T to_int(const std::string& input) {
+    T ret = 0;
     std::stringstream ostr;
     ostr << input;
     ostr >> ret;
--- samples/to_int.cpp
+++ samples/to_int.cpp
@@ -5,7 +5,11 @@ using namespace gdp::gdu;
 using namespace std;
 
 int main() {
-    cout << to_int("123") << endl;
-    cout << to_int("1231afdal") << endl;
+    cout << to_int<int>("123") << endl;
+    cout << to_int<uint32_t>("1231afdal") << endl;
+    cout << to_int<uint64_t>("1234567890123456") << endl;
+    uint32_t ret = to_int<uint64_t>("1234567890123456");
+//    uint64_t ret = to_int<uint64_t>("1234567890123456");
+    cout << ret << endl;
     return 0;
 }
