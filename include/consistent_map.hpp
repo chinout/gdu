81f3df014a78f680a7673eb223a8dc59308c80c7
yangyanbin <yangyanbin@shandagames.com>
2018-03-20 13:28:33 +0800

    add consistent_map

---
+++ include/consistent_map.hpp
@@ -0,0 +1,67 @@
+// copyright 2017 war10ck
+
+#ifndef GDU_CONSISTENT_MAP_HPP_
+#define GDU_CONSISTENT_MAP_HPP_
+
+#include <thread>
+#include <mutex>
+#include <map>
+#include <algorithm>
+#include <utility>
+#include <functional>
+#include "hash.hpp"
+
+namespace gdp {
+namespace gdu {
+
+template <typename K, typename V>
+class ConsistentMap {
+ public:
+    ConsistentMap() = default;
+    virtual ~ConsistentMap() = default;
+    ConsistentMap(const ConsistentMap<K, V>&) = delete;
+    ConsistentMap<K, V>& operator = (const ConsistentMap<K, V>&) = delete;
+
+    bool insert(const K& hash, const V& node) {
+        return data_.insert(std::make_pair(hash, node)).second;
+    }
+
+    void update(const K& hash, const V& node) {
+        data_[hash] = node;
+    }
+
+    bool find(const K& hash, V& node) {
+        if (data_.empty()) {
+            return false;
+        }
+
+        auto it = data_.lower_bound(hash);
+
+        if (it == data_.end()) {
+            it = data_.begin();
+        }
+
+        node = it->second;
+        return true;
+    }
+
+    size_t erase(uint32_t hash) {
+        return data_.erase(hash);
+    }
+
+    bool empty() {
+        return data_.empty();
+    }
+
+    size_t size() {
+        return data_.size();
+    }
+
+ private:
+    std::map<K, V> data_;
+};
+
+}  // namespace gdu 
+}  // namespace gdp
+
+#endif  // GDU_CONSISTENT_MAP_HPP_
---
+++ samples/consistent_map.cpp
@@ -0,0 +1,19 @@
+#include "consistent_map.hpp"
+#include "hash.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace gdp::gdu;
+
+int main() {
+    string nodes[] = {"127.0.0.1:1234", "127.0.0.1:1235", "127.0.0.1:1236"};
+    ConsistentMap<uint32_t, string> consistent_map;
+    for (auto& node : nodes) {
+        uint32_t hash = HashCode(node, 3);
+        std::cout << hash << std::endl;
+        consistent_map.insert(hash, node);
+    }
+
+    
+    return 0;
+}
